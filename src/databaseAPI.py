a2889312698fda7da6911e46dc0dcafd89a0e5e9
Syed Zoraiz Sabeel <zoraizzaaina@gmail.com>
2026-05-05 06:15:23 -0700

    this is the select method that gets all members

--- src/databaseAPI.py
+++ src/databaseAPI.py
@@ -4,4 +4,12 @@ from config import DB_CONFIG
 def get_db_connection():
     return mysql.connector.connect(**DB_CONFIG)
 
+def get_all_members():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True) 
+    cursor.execute("SELECT * FROM Members")
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
 
